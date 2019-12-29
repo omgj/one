f3a72326dcfe84f34f4bc9d5cccd1ae3364a6731
omgj <olivermgjohnson@gmail.com>
2019-12-29 12:23:51 +1100

    etse

--- main.go
+++ main.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 )
+
+func yi(w http.ResponseWriter, r *http.Request) {
+	b, _ := ioutil.ReadFile("index.html")
+	io.WriteString(w, string(b))
+}
 
 func main() {
 	http.Handle("/stuff/", http.StripPrefix("/stuff/", http.FileServer(http.Dir("stuff"))))
@@ -14,5 +21,6 @@ func main() {
 	if port == "" {
 		port = "8080"
 	}
+	log.Println("listening on 8080")
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
